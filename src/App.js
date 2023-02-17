16deb518f39c933c35d5ec0dd1d3f6d9cd60d201
Nefedov Sergey <nefedov38@gmail.com>
2023-02-17 17:41:00 +0300

    add animation

--- src/App.js
+++ src/App.js
@@ -59,6 +59,7 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+
       {sortedAndSearchedPost.length !== 0 ? (
         <PostList
           posts={sortedAndSearchedPost}
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import s from "../App.module.css";
 import PostItem from "./PostItem";
+import "./PostList.css";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const PostList = ({ removePost, ...props }) => {
   return (
-    <div className={s.postList}>
-      {props.posts.map((item, index) => (
-        <PostItem {...item} number={index + 1} removePost={removePost} />
-      ))}
+    <div>
+      <TransitionGroup className={s.postList}>
+        {props.posts.map((item, index) => (
+          <CSSTransition key={item.id} timeout={500} classNames="post">
+            <PostItem {...item} number={index + 1} removePost={removePost} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
